9ce0e551993a00e104aef54f6a69246a191278c7
Pontus Stjernström <pontus@wmyl.se>
2018-11-27 07:47:59 +0100

    Add MeleeWeapon class for easier dealing with damage

---
+++ druidgame/model/MeleeWeapon.cpp
@@ -0,0 +1,9 @@
+//
+//  Weapon.cpp
+//  druidgame
+//
+//  Created by Pontus Stjernström on 2018-11-27.
+//  Copyright © 2018 nocroft. All rights reserved.
+//
+
+#include "Weapon.h"
---
+++ druidgame/model/MeleeWeapon.h
@@ -0,0 +1,20 @@
+//
+//  Weapon.hpp
+//  druidgame
+//
+//  Created by Pontus Stjernström on 2018-11-27.
+//  Copyright © 2018 nocroft. All rights reserved.
+//
+
+#ifndef Weapon_h
+#define Weapon_h
+
+#include <stdio.h>
+
+class MeleeWeapon {
+    
+public:
+    Weapon(int dmg, int range, int haste)
+}
+
+#endif /* Weapon_h */
