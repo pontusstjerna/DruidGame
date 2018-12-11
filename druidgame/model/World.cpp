c2477f8f38e9ee63c94895b6f31be8e1e2cc4739
Pontus Stjernström <pontus@wmyl.se>
2018-12-11 08:02:41 +0100

    Worked more on collision correction

--- druidgame/model/World.cpp
+++ druidgame/model/World.cpp
@@ -1,6 +1,7 @@
 #include "World.h"
 #include <stdio.h>
 #include "../util/Geometry.h"
+#include <cmath>
 
 World::World()
 {
@@ -90,10 +91,11 @@ void World::collideCharacter(Character* object, float dTime) {
     int pBottom = object->getY() + object->getHeight() + object->getYVel() * dTime;
 
 	//printf("LS: %i\n", pLeft);
-    bool intersection = false;
+    bool hasCollided = false;
+    bool collisions[4];
 
 	//Loop through all blocks
-	for (int i = 0; i < activeMap->getNumberBlocks() && !intersection; i++) {
+	for (int i = 0; i < activeMap->getNumberBlocks() /*&& !intersection*/; i++) {
         Block* block = activeMap->getBlocks()[i];
         
 		//Nexts
@@ -104,46 +106,54 @@ void World::collideCharacter(Character* object, float dTime) {
 
 		bool betweenVertical = bTop < pBottom && bBottom > pTop;
 		bool betweenHorizontal = bLeft < pRight && bRight > pLeft;
-
-        bool collisions[4];
         
         collisions[Character::LEFT] = pLeft <= bRight && pRight >= bRight && betweenVertical;
         collisions[Character::RIGHT] = pRight >= bLeft && pLeft <= bLeft && betweenVertical;
         collisions[Character::TOP] = pTop <= bBottom && pBottom >= bBottom && betweenHorizontal;
         collisions[Character::BOTTOM] = pBottom >= bTop && pTop <= bTop && betweenHorizontal;
         
-        intersection = collisions[0] || collisions[1] || collisions[2] || collisions[3];
+        bool intersection = collisions[0] || collisions[1] || collisions[2] || collisions[3];
+        hasCollided = intersection || hasCollided;
         
         if (intersection) {
             resolveCollision(object, block, collisions);
-        } else {
-            object->clearCollisions();
         }
 	}
+    
+    if (!hasCollided) {
+        object->clearCollisions();
+    }
 }
 
 void World::resolveCollision(Character* object, Block* block, bool collisions[4]) {
     float objCenterX = object->getX() + object->getWidth() / 2;
-    float objCenterY = object->getY() + (object->getHeight() / 2);
+    float objCenterY = object->getY() + ((object->getHeight()) / 2);
     
     float blockCenterX = block->getX() + block->getWidth() / 2;
     float blockCenterY = block->getY() + block->getHeight() / 2;
     
+    float aw = (object->getWidth() + block->getWidth()) / 2;
+    float ah = (object->getHeight() + block->getHeight()) / 2;
+    
     float vectorX = objCenterX - blockCenterX;
     float vectorY = objCenterY - blockCenterY;
     
+    if (abs(vectorX) > aw || abs(vectorY) > ah) {
+        return;
+    }
+    
     // Is the vector y longer than vector x?
-    if (vectorY * vectorY < vectorX * vectorX) { // Collision top or bottom
+    if (abs(vectorY / block->getHeight()) > abs(vectorX / block->getWidth())) { // Collision top or bottom
         if (vectorY > 0) { // VectorY pointing down
             object->collide(collisions, block->getY() + block->getHeight() + 1);
         } else { // VectorY pointing up
             object->collide(collisions, block->getY() - object->getHeight());
         }
     } else {
-        if (vectorX > 0) { // VectorY pointing right
-            object->collide(collisions, block->getX() + block->getWidt() + 1);
-        } else { // VectorY pointing up
-            object->collide(collisions, block->getY() - object->getHeight());
+        if (vectorX > 0) { // VectorX pointing right
+            object->collide(collisions, block->getX() + block->getWidth() + 1);
+        } else { // VectorX pointing left
+            object->collide(collisions, block->getX() - object->getWidth());
         }
     }
 }
